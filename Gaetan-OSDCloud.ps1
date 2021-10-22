298ae4b276d4b782fd5ec4f7e57d9353091aec8f
Gaetan <32365616+gvillant@users.noreply.github.com>
2021-10-22 10:08:21 +0200

    Update Gaetan-OSDCloud.ps1
    Add Custom Generic images support with Azure File Share.

--- Gaetan-OSDCloud.ps1
+++ Gaetan-OSDCloud.ps1
@@ -17,11 +17,13 @@ Write-Host "2: Zero-Touch Win10 21H1 | French  | Enterprise"-ForegroundColor Yel
 Write-Host "3: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
 Write-Host "4: Give me more choice ... "-ForegroundColor Yellow
 Write-Host "5: Start-OSDCloudGUI "-ForegroundColor Yellow
-Write-Host "6: Exit`n"-ForegroundColor Yellow
+Write-Host "6: Start-OSDCloudWIM (DELL Generic WIMs)"-ForegroundColor Yellow
+Write-Host "7: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
 
 Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
 
+<#
 #Install-Module OSD -Force
 #Import-Module OSD -Force
 
@@ -35,6 +37,19 @@ function Invoke-ParseDate {
     Get-Date -Year $Array[2] -Month $Array[0] -Day $Array[1]
 }
 
+#> 
+
+#Connect Azure Storage file share for custom DELL WIM access
+$connectTestResult = Test-NetConnection -ComputerName osdcloud.file.core.windows.net -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"osdcloud.file.core.windows.net`" /user:`"localhost\osdcloud`" /pass:`"BckU9jeGTOtkSjQ56byVkbFYYTFkvtqte2NPPpjt5bsuK930Licjim7R39/FGzs3GxTmot3r7wLT2g62+pRlLg==`""
+    # Mount the drive
+    New-PSDrive -Name O -PSProvider FileSystem -Root "\\osdcloud.file.core.windows.net\osdcloud-fs" -Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
+
 switch ($input)
 {
     '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
@@ -42,7 +57,8 @@ switch ($input)
     '3' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
     '4' { Start-OSDCloud } 
     '5' { Start-OSDCloudGUI } 
-    '6' { Exit }
+    '6' { Start-OSDCloudWIM } 
+    '7' { Exit }
 }
 
 wpeutil reboot
