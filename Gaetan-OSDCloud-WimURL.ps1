b1c861a24236b9ad8a38e766bf9230b3512d10ea
Gaetan <32365616+gvillant@users.noreply.github.com>
2021-10-25 16:26:15 +0200

    Create Gaetan-OSDCloud-WimURL.ps1

---
+++ Gaetan-OSDCloud-WimURL.ps1
@@ -0,0 +1,33 @@
+cls
+Write-Host "================ Main Menu ==================" -ForegroundColor Yellow
+Write-Host " "
+Write-Host "       _____    ______   _        _      " -ForegroundColor Cyan
+Write-Host "      |  __ \  |  ____| | |      | |     " -ForegroundColor Cyan
+Write-Host "      | |  | | | |__    | |      | |     " -ForegroundColor Cyan
+Write-Host "      | |  | | |  __|   | |      | |     " -ForegroundColor Cyan
+Write-Host "      | |__| | | |____  | |____  | |____ " -ForegroundColor Cyan
+Write-Host "      |_____/  |______| |______| |______|" -ForegroundColor Cyan
+Write-Host " "
+Write-Host "=============================================" -ForegroundColor Yellow
+Write-Host "============== @gaetanvillant ===============" -ForegroundColor Yellow
+Write-Host "=============================================`n" -ForegroundColor Yellow
+
+Write-Host "`n DISCLAIMER: USE AT YOUR OWN RISK - Going further will erase all data on your disk ! `n"-ForegroundColor Red
+
+#Prompt for confirmation
+$title    = 'Warning'
+$question = 'Are you sure you want to proceed? All data will be deleted and a new OS will be installed'
+$choices  = '&Yes', '&No'
+
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    Write-Host 'confirmed'
+} else {
+    Write-Host 'cancelled'
+    exit
+}
+
+Start-OSDCloud -ImageFileUrl http://osd.gaetanvillant.com:8888/20h2.wim -ImageIndex 1 -Zti
+
+#reboot to OS
+wpeutil reboot
