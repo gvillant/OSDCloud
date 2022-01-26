aa14d6ed0be714d217c01261f8ef1fb68441c117
Gaetan <32365616+gvillant@users.noreply.github.com>
2022-01-26 11:28:54 +0100

    Update Gaetan-OSDCloud.ps1

--- Gaetan-OSDCloud.ps1
+++ Gaetan-OSDCloud.ps1
@@ -45,7 +45,7 @@ switch ($input)
         Expand-Archive $GenericPPKGDestPath\GenericPPKG.zip $GenericPPKGDestPath
         #Stage Audit_unattend file 
         Save-WebFile -SourceURL $AuditUnattendXML -DestinationName "Unattend.xml" -DestinationDirectory "C:\Windows\panther\Unattend"
-        read-host "Press ENTER to continue..."        
+        #read-host "Press ENTER to continue..."        
         } 
     '4' { #Win11
         $Global:StartOSDCloudGUI = $null
