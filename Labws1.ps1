f533080c6d6eed63e0138a47070173c9a272b3d5
Gaetan <32365616+gvillant@users.noreply.github.com>
2022-02-21 15:15:40 +0100

    Update Labws1.ps1
    Add 3.3 + French

--- Labws1.ps1
+++ Labws1.ps1
@@ -6,8 +6,11 @@ Write-Host "============== @gaetanvillant ===============" -ForegroundColor Yell
 Write-Host "=============================================`n" -ForegroundColor Yellow
 Write-Host "1: Win10 21H1 | English | Enterprise (Windows Update ESD file)" -ForegroundColor Yellow
 Write-Host "2: Win10 20H2 | English | Enterprise (Windows Update ESD file) + WS1 DS Online 3.2 + WinRE" -ForegroundColor Yellow
-Write-Host "3: Win11 | English | Enterprise (Windows Update ESD file)" -ForegroundColor Yellow
-Write-Host "4: Start the graphical OSDCloud (Start-OSDCloudGUI)" -ForegroundColor Yellow
+Write-Host "3: Win10 20H2 | English | Enterprise (Windows Update ESD file) + WS1 DS Online 3.3 + WinRE" -ForegroundColor Yellow
+Write-Host "4: Win10 20H2 | French | Enterprise (Windows Update ESD file) + WS1 DS Online 3.3 + WinRE" -ForegroundColor Yellow
+Write-Host "5: Win11 | English | Enterprise (Windows Update ESD file)" -ForegroundColor Yellow
+Write-Host "6: Win11 | English | Enterprise (Windows Update ESD file) + WS1 DS Online 3.3 + WinRE" -ForegroundColor Yellow
+Write-Host "8: Start the graphical OSDCloud (Start-OSDCloudGUI)" -ForegroundColor Yellow
 Write-Host "9: Exit`n"-ForegroundColor Yellow
 
 Write-Host "`n DISCLAIMER: USE AT YOUR OWN RISK - Going further will erase all data on your disk ! `n"-ForegroundColor Red
@@ -63,6 +66,18 @@ function Install-WS1DropShipOnline32 {
         #read-host "Press ENTER to continue..."        
 }
 
+function Install-WS1DropShipOnline33 {
+	$GenericPPKGURL = "http://osd.gaetanvillant.com:8888/_WS1/GenericPPKG_3-3.zip"
+        $AuditUnattendXML = "https://raw.githubusercontent.com/gvillant/OSDCloud/main/unattend_ws1_DropShip.xml"
+        $GenericPPKGDestPath = "C:\Temp\VMware"
+        #Get Dropship Generic PPKG
+        Save-WebFile -SourceURL $GenericPPKGURL -DestinationName "GenericPPKG.zip" -DestinationDirectory $GenericPPKGDestPath
+        Expand-Archive $GenericPPKGDestPath\GenericPPKG.zip $GenericPPKGDestPath
+        #Stage Audit_unattend file 
+        Save-WebFile -SourceURL $AuditUnattendXML -DestinationName "Unattend.xml" -DestinationDirectory "C:\Windows\panther\Unattend"
+        #read-host "Press ENTER to continue..."        
+}
+
 switch ($input)
 {
     '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI 
@@ -71,9 +86,54 @@ switch ($input)
     '2' { 
         Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
         Install-WS1DropShipOnline32
+        Create-WinREPartition       
+    '3' { 
+        Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+        Install-WS1DropShipOnline33
         Create-WinREPartition   
         } 
-    '3' { #Win11
+    '4' { 
+        Start-OSDCloud -OSLanguage fr-fr -OSBuild 20H2 -OSEdition Enterprise -ZTI
+        Install-WS1DropShipOnline33
+        Create-WinREPartition
+	}
+    '5' { #Win11
+        $Global:StartOSDCloudGUI = $null
+        $Global:StartOSDCloudGUI = [ordered]@{
+            ApplyManufacturerDrivers    = $true
+            ApplyCatalogDrivers         = $false
+            ApplyCatalogFirmware        = $false
+            AutopilotJsonChildItem      = $false
+            AutopilotJsonItem           = $false
+            AutopilotJsonName           = $false
+            AutopilotJsonObject         = $false
+            AutopilotOOBEJsonChildItem  = $false
+            AutopilotOOBEJsonItem       = $false
+            AutopilotOOBEJsonName       = $false
+            AutopilotOOBEJsonObject     = $false
+            ImageFileFullName           = $false
+            ImageFileItem               = $false
+            ImageFileName               = $false
+            #Manufacturer                = $formMainWindowControlCSManufacturerTextbox.Text
+            OOBEDeployJsonChildItem     = $false
+            OOBEDeployJsonItem          = $false
+            OOBEDeployJsonName          = $false
+            OOBEDeployJsonObject        = $false
+            OSBuild                     = '21H2'
+            OSEdition                   = 'Enterprise'
+            OSImageIndex                = 1
+            OSLanguage                  = 'en-us'
+            OSLicense                   = 'Volume'
+            OSVersion                   = 'Windows 11'
+            #Product                     = $formMainWindowControlCSProductTextbox.Text
+            Restart                     = $true
+            SkipAutopilot               = $false
+            SkipAutopilotOOBE           = $false
+            SkipODT                     = $true
+            SkipOOBEDeploy              = $false
+            ZTI                         = $true
+            }
+    '6' { #Win11 + WS1 3.3
         $Global:StartOSDCloudGUI = $null
         $Global:StartOSDCloudGUI = [ordered]@{
             ApplyManufacturerDrivers    = $true
@@ -111,8 +171,10 @@ switch ($input)
             }
         #$Global:StartOSDCloudGUI | Out-Host
         Start-OSDCloud
+	Install-WS1DropShipOnline33
+        Create-WinREPartition
         }
-    '4' { Start-OSDCloudGUI } 
+    '8' { Start-OSDCloudGUI } 
     '9' { Exit }
 }
 
